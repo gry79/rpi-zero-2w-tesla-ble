02cf8bff9b7ad9ffdb537e5b130732e6d096b2c6
Gry <gry79@users.noreply.github.com>
2024-08-24 20:38:42 +0200

    Imporved MQTT wrapper script

--- tesla-mqtt.sh
+++ tesla-mqtt.sh
@@ -6,12 +6,22 @@ TESLA_BIN_DIR=/home/pi/bin/tesla
 
 source ${TESLA_BIN_DIR}/tesla-mqtt.properties
 
+if [ -z "${TOPIC_ID}" ]; then
+    MAIN_TOPIC="tesla/command"
+else
+    MAIN_TOPIC="tesla/command/${TOPIC_ID}"
+fi
+
 function ctrl_c() {
     echo "Exiting!"
     exit 0
 }
 
-mosquitto_pub -h "${MQTT_BROKER}" -t "tesla/command/${TOPIC_ID}/info" -m "{ \"ip\": \"$(ifconfig wlan0 2>/dev/null | grep "inet " | awk '{print $2}')\", \"hostname\": \"$(hostname -f)\", \"model\":\"$(tr -d '\0' </proc/device-tree/model)\" }"
+echo "Tesla VIN   : ${TESLA_VIN}"
+echo "MQTT Breoker: ${MQTT_BROKER}"
+echo "Topic       : ${MAIN_TOPIC}"
+
+mosquitto_pub -h "${MQTT_BROKER}" -t "${MAIN_TOPIC}/info" -m "{ \"ip\": \"$(ifconfig wlan0 2>/dev/null | grep "inet " | awk '{print $2}')\", \"hostname\": \"$(hostname -f)\", \"model\":\"$(tr -d '\0' </proc/device-tree/model)\" }"
 if [ "$?" -ne "0" ]; then
     echo "ERROR sending message to MQTT broker, exiting"
     sleep 5
@@ -20,15 +30,15 @@ fi
 
 while true  # Keep an infinite loop to reconnect when connection lost/broker unavailable
 do
-    mosquitto_sub -h "${MQTT_BROKER}" -t "tesla/command/${TOPIC_ID}" | while read -r payload
+    mosquitto_sub -h "${MQTT_BROKER}" -t "${MAIN_TOPIC}" | while read -r PAYLOAD
     do
         # Here is the callback to execute whenever you receive a message:
-        echo "Tesla command: ${payload}"
-        if [ "${payload}" = "add-key-request" ]; then
+        echo "Tesla command: ${PAYLOAD}"
+        if [ "${PAYLOAD}" = "add-key-request" ]; then
             RESPONSE=$(${TESLA_BIN_DIR}/tesla-control -vin ${TESLA_VIN} -ble add-key-request ${TESLA_BIN_DIR}/public.pem owner cloud_key 2>&1)
             RES=$?
         else
-            RESPONSE=$(${TESLA_BIN_DIR}/tesla-control -vin ${TESLA_VIN} -ble -key-name private.pem -key-file ${TESLA_BIN_DIR}/private.pem ${payload} 2>&1)
+            RESPONSE=$(${TESLA_BIN_DIR}/tesla-control -vin ${TESLA_VIN} -ble -key-name private.pem -key-file ${TESLA_BIN_DIR}/private.pem ${PAYLOAD} 2>&1)
             RES=$?
         fi
         if [ "${#RESPONSE}" -eq "0" ]; then
@@ -39,11 +49,11 @@ do
             RESPONSE=\"${RESPONSE}\"
         fi
         if [ $RES -eq 0 ]; then
-            echo "OK command: ${payload}"
-            mosquitto_pub -h "${MQTT_BROKER}" -t "tesla/command/${TOPIC_ID}/response" -m "{ \"status\": \"OK\", \"response\": ${RESPONSE} }"
+            STATUS="OK"
         else
-            echo "ERROR command: ${payload}"
-            mosquitto_pub -h "${MQTT_BROKER}" -t "tesla/command/${TOPIC_ID}/response" -m "{ \"status\": \"ERROR\", \"response\": ${RESPONSE} }"
+            STATUS="ERROR"
         fi
+        echo "${STATUS} command: ${PAYLOAD}"
+        mosquitto_pub -h "${MQTT_BROKER}" -t "${MAIN_TOPIC}/response" -m "{ \"status\": \"${STATUS}\", \"response\": ${RESPONSE} }"
     done
 done
