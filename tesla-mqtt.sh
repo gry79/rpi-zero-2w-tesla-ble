44207ec80ffd035ab229da702f0172e1030fbfe4
Gry <gry79@users.noreply.github.com>
2024-08-25 21:30:38 +0200

    Added some more information to the initial message

--- tesla-mqtt.sh
+++ tesla-mqtt.sh
@@ -21,7 +21,30 @@ echo "Tesla VIN   : ${TESLA_VIN}"
 echo "MQTT Breoker: ${MQTT_BROKER}"
 echo "Topic       : ${MAIN_TOPIC}"
 
-mosquitto_pub -h "${MQTT_BROKER}" -t "${MAIN_TOPIC}/info" -m "{ \"ip\": \"$(ifconfig wlan0 2>/dev/null | grep "inet " | awk '{print $2}')\", \"hostname\": \"$(hostname -f)\", \"model\":\"$(tr -d '\0' </proc/device-tree/model)\" }"
+VC=$(vcgencmd get_throttled)
+UNDERVOLTAGE_DETECTED=$([[ "$(($((16#${VC:12})) & 1))" -eq "0" ]] && echo "no" || echo "yes")
+ARM_FREQUENCY_CAPPED=$([[ "$(($((16#${VC:12})) & 2))" -eq "0" ]] && echo "no" || echo "yes")
+CURRENTLY_THROTTLED=$([[ "$(($((16#${VC:12})) & 4))" -eq "0" ]] && echo "no" || echo "yes")
+SOFT_TEMPERATURE_LIMIT_ACTIVE=$([[ "$(($((16#${VC:12})) & 8))" -eq "0" ]] && echo "no" || echo "yes")
+UNDERVOLTAGE_OCCURED=$([[ "$(($((16#${VC:12})) & 65536))" -eq "0" ]] && echo "no" || echo "yes")
+ARM_FREQUENCY_CAPPED_OCCURED=$([[ "$(($((16#${VC:12})) & 131072))" -eq "0" ]] && echo "no" || echo "yes")
+THROTTLING_OCCURED=$([[ "$(($((16#${VC:12})) & 262144))" -eq "0" ]] && echo "no" || echo "yes")
+SOFT_TEMPERATURE_LIMIT_OCCURED=$([[ "$(($((16#${VC:12})) & 524288))" -eq "0" ]] && echo "no" || echo "yes")
+
+mosquitto_pub -h "${MQTT_BROKER}" -t "${MAIN_TOPIC}/info" -m "{ \
+\"ip\": \"$(ifconfig wlan0 2>/dev/null | grep "inet " | awk '{print $2}')\", \
+\"hostname\": \"$(hostname -f)\", \"model\":\"$(tr -d '\0' </proc/device-tree/model)\", \
+\"vin\": \"${TESLA_VIN}\", \
+\"undervoltage_detected\": \"${UNDERVOLTAGE_DETECTED}\", \
+\"arm_frequency_capped\": \"${ARM_FREQUENCY_CAPPED}\", \
+\"currently_throttled\": \"${CURRENTLY_THROTTLED}\", \
+\"soft_temperature_limit_active\": \"${SOFT_TEMPERATURE_LIMIT_ACTIVE}\", \
+\"undervoltage_occured\": \"${UNDERVOLTAGE_OCCURED}\", \
+\"arm_frequency_capped_occured\": \"${ARM_FREQUENCY_CAPPED_OCCURED}\", \
+\"throttling_occured\": \"${THROTTLING_OCCURED}\", \
+\"soft_temperature_limit_occured\": \"${SOFT_TEMPERATURE_LIMIT_OCCURED}\", \
+\"uptime\": \"$(uptime -p)\" \
+}"
 if [ "$?" -ne "0" ]; then
     echo "ERROR sending message to MQTT broker, exiting"
     sleep 5
