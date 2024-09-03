64de749329ec1b2f6a67d8dccf8f8308c805e86f
Gry <gry79@users.noreply.github.com>
2024-09-03 19:59:13 +0200

    Fixed few bugs

--- setup.sh
+++ setup.sh
@@ -6,31 +6,39 @@ ARCH=arm64
 TESLA_BIN_DIR=${HOME}/bin/tesla
 GIT_REPO_DIR=${HOME}/git
 
+do_exec () {
+    $@
+    if [ "$?" -ne "0" ]; then
+        echo "ERROR executing command: $@"
+        exit 1
+    fi
+}
+
 disable_service () {
     local SERVICE_NAME=$1
-    $(sudo systemctl is-active --quiet ${SERVICE_NAME})
+    sudo systemctl is-active --quiet ${SERVICE_NAME}
     if [ "$?" -eq "0" ]; then
         echo "### Stopping service ${SERVICE_NAME}"
-        sudo systemctl stop ${SERVICE_NAME}
+        do_exec sudo systemctl stop ${SERVICE_NAME}
     fi
-    $(sudo systemctl is-enabled --quiet ${SERVICE_NAME})
+    sudo systemctl is-enabled --quiet ${SERVICE_NAME}
     if [ "$?" -eq "0" ]; then
         echo "### Disabling service ${SERVICE_NAME}"
-        sudo systemctl disable ${SERVICE_NAME}
+        do_exec sudo systemctl disable ${SERVICE_NAME}
     fi
 }
 
 enable_service () {
     local SERVICE_NAME=$1
-    $(sudo systemctl is-enabled --quiet ${SERVICE_NAME})
+    sudo systemctl is-enabled --quiet ${SERVICE_NAME}
     if [ "$?" -ne "0" ]; then
         echo "### Enabling service ${SERVICE_NAME}"
-        sudo systemctl enable ${SERVICE_NAME}
+        do_exec sudo systemctl enable ${SERVICE_NAME}
     fi
-    $(sudo systemctl is-active --quiet ${SERVICE_NAME})
+    sudo systemctl is-active --quiet ${SERVICE_NAME}
     if [ "$?" -ne "0" ]; then
         echo "### Starting service ${SERVICE_NAME}"
-        sudo systemctl start ${SERVICE_NAME}
+        do_exec sudo systemctl start ${SERVICE_NAME}
     fi
 }
 
@@ -72,12 +80,12 @@ export LC_ALL=en_GB.UTF-8
 export LANGUAGE=en_GB.UTF-8
 
 echo "### Updating system and install needed dependencies"
-sudo apt update
-sudo apt upgrade -y
-sudo apt install mc git jq build-essential mosquitto-clients -y
+do_exec sudo apt update
+do_exec sudo apt upgrade -y
+do_exec sudo apt install mc git jq build-essential mosquitto-clients -y
 
 echo "### Disable man-db auto-update"
-sudo rm /var/lib/man-db/auto-update
+do_exec sudo rm /var/lib/man-db/auto-update
 
 if [ ! -f ${HOME}/.ssh/id_rsa ]; then
     echo "### Creating user keypair"
@@ -86,26 +94,27 @@ fi
 
 enable_service dphys-swapfile.service
 echo "### Temporary bigger Swap to compile tesla-control with GO"
-sudo dphys-swapfile swapoff
+do_exec sudo dphys-swapfile swapoff
 sudo tee /etc/dphys-swapfile > /dev/null <<EOT
 CONF_SWAPSIZE=2048
 EOT
-sudo dphys-swapfile setup
-sudo dphys-swapfile swapon
+do_exec sudo dphys-swapfile setup
+do_exec sudo dphys-swapfile swapon
 
 if [ -d "$HOME/.local/share/go" ]; then
     echo "### Uninstalling old GoLang"
-    sudo rm -rf $HOME/.local/share/go
+    do_exec sudo rm -rf $HOME/.local/share/go
 fi
 
 echo "### Installing GoLang ${GO_VERSION}"
 cd $HOME
-wget https://dl.google.com/go/go${GO_VERSION}.linux-${ARCH}.tar.gz
-mkdir -p $HOME/.local/share
-tar -C $HOME/.local/share -xzf go${GO_VERSION}.linux-${ARCH}.tar.gz
-rm -f go${GO_VERSION}.linux-${ARCH}.tar.gz
+do_exec wget https://dl.google.com/go/go${GO_VERSION}.linux-${ARCH}.tar.gz
+do_exec mkdir -p $HOME/.local/share
+do_exec tar -C $HOME/.local/share -xzf go${GO_VERSION}.linux-${ARCH}.tar.gz
+do_exec rm -f go${GO_VERSION}.linux-${ARCH}.tar.gz
 
-if [ "$(grep -i -q share/go ${HOME}/.bashrc)" -ne "0" ]; then
+$(grep -i -q share/go ${HOME}/.bashrc)
+if [ "$?" -ne "0" ]; then
     echo >> "${HOME}/.bashrc"
     echo 'export GOPATH=$HOME/.local/share/go' >> "${HOME}/.bashrc"
     echo 'export PATH=$HOME/.local/share/go/bin:/home/pi/bin/tesla:$PATH' >> "${HOME}/.bashrc"
@@ -117,43 +126,43 @@ if [ -d "${GIT_REPO_DIR}" ]; then
     sudo rm -rf ${GIT_REPO_DIR}
 fi
 echo "### Checkout Tesla vehicle-command project from GitHub"
-mkdir -p ${GIT_REPO_DIR}
+do_exec mkdir -p ${GIT_REPO_DIR}
 cd ${GIT_REPO_DIR}
-git clone https://github.com/teslamotors/vehicle-command.git
+do_exec git clone https://github.com/teslamotors/vehicle-command.git
 
 echo "### Compiling tesla-control"
 cd ${GIT_REPO_DIR}/vehicle-command/cmd/tesla-control
-go build
-mkdir -p ${TESLA_BIN_DIR}
-cp tesla-control ${TESLA_BIN_DIR}
+do_exec go build
+do_exec mkdir -p ${TESLA_BIN_DIR}
+do_exec cp tesla-control ${TESLA_BIN_DIR}
 cd ${TESLA_BIN_DIR}
-rm -rf ${GIT_REPO_DIR}
+do_exec rm -rf ${GIT_REPO_DIR}
 
 echo "### Uninstalling GoLang"
-sudo rm -rf $HOME/.local/share/go
+do_exec sudo rm -rf $HOME/.local/share/go
 
 if [ ! -f private.pem ]; then
     echo "### Creating private key needed for Tesla vehicle"
-    openssl ecparam -genkey -name prime256v1 -noout > private.pem
+    do_exec openssl ecparam -genkey -name prime256v1 -noout > private.pem
 fi
 if [ ! -f public.pem ]; then
     echo "### Creating public key needed for Tesla vehicle"
-    openssl ec -in private.pem -pubout > public.pem
+    do_exec openssl ec -in private.pem -pubout > public.pem
 fi
 
 echo "### Downloading MQTT wrapper script"
-curl -o ${TESLA_BIN_DIR}/tesla-mqtt.sh https://raw.githubusercontent.com/gry79/rip-zero-2w-tesla-ble/main/tesla-mqtt.sh -L
-chmod 0755 ${TESLA_BIN_DIR}/tesla-mqtt.sh
+do_exec curl -o ${TESLA_BIN_DIR}/tesla-mqtt.sh https://raw.githubusercontent.com/gry79/rip-zero-2w-tesla-ble/main/tesla-mqtt.sh -L
+do_exec chmod 0755 ${TESLA_BIN_DIR}/tesla-mqtt.sh
 
 tee ${TESLA_BIN_DIR}/tesla-mqtt.properties > /dev/null <<EOT
 TESLA_VIN=${TESLA_VIN}
 MQTT_BROKER=${MQTT_BROKER}
 TOPIC_ID=${TESLA_VIN}
 EOT
-chmod 0644 ${TESLA_BIN_DIR}/tesla-mqtt.properties
+do_exec chmod 0644 ${TESLA_BIN_DIR}/tesla-mqtt.properties
 
 echo "### Allowing tesla-control binary to access Bluetooth"
-sudo setcap 'cap_net_admin=eip' "${TESLA_BIN_DIR}/tesla-control"
+do_exec sudo setcap 'cap_net_admin=eip' "${TESLA_BIN_DIR}/tesla-control"
 
 disable_service dphys-swapfile.service
 disable_service apt-daily.timer
@@ -161,19 +170,21 @@ disable_service apt-daily-upgrade.timer
 disable_service bluetooth.service
 disable_service hciuart.service
 disable_service polkit.service
+disable_service avahi-daemon.socket
 disable_service avahi-daemon.service
 
 echo "### Removing avahi-daemon"
-sudo apt remove --purge avahi-daemon -y
+do_exec sudo apt remove --purge avahi-daemon -y
+do_exec sudo rm -rf /run/avahi-daemon
 
 echo "### Removing modemmanager"
-sudo apt remove --purge modemmanager -y
+do_exec sudo apt remove --purge modemmanager -y
 
 echo "### Removing bluez"
-sudo apt remove --purge bluez -y
+do_exec sudo apt remove --purge bluez -y
 
 echo "### Removing triggerhappy"
-sudo apt remove --purge triggerhappy -y
+do_exec sudo apt remove --purge triggerhappy -y
 
 echo "### Setting minimalistic journald config"
 sudo tee /etc/systemd/journald.conf > /dev/null <<EOT
@@ -202,8 +213,8 @@ WantedBy=multi-user.target
 EOT
 
 echo "### Installing welcome script on logon"
-sudo curl -o /etc/profile.d/xinfo.sh https://raw.githubusercontent.com/gry79/rip-zero-2w-tesla-ble/main/xinfo.sh -L
-sudo chmod 0755 /etc/profile.d/xinfo.sh
+do_exec sudo curl -o /etc/profile.d/xinfo.sh https://raw.githubusercontent.com/gry79/rip-zero-2w-tesla-ble/main/xinfo.sh -L
+do_exec sudo chmod 0755 /etc/profile.d/xinfo.sh
 
 echo "### Setting up some housekeeping cron jobs to free some valuable space"
 tee /tmp/c1 > /dev/null <<EOT
@@ -212,27 +223,27 @@ tee /tmp/c1 > /dev/null <<EOT
 0 0 * * * history -c && history -w
 0 0 * * * sudo apt clean
 EOT
-crontab /tmp/c1
-rm -f /tmp/c1
+do_exec crontab /tmp/c1
+do_exec rm -f /tmp/c1
 
 echo "### Setting boot partition read-only to prevent SD card failure"
-sudo awk '$2~"^/boot/firmware$" && $4~"^defaults$"{$4=$4",ro"}1' OFS="\t" /etc/fstab > /tmp/fstab
-sudo mv -f /tmp/fstab /etc/fstab
+do_exec sudo awk '$2~"^/boot/firmware$" && $4~"^defaults$"{$4=$4",ro"}1' OFS="\t" /etc/fstab > /tmp/fstab
+do_exec sudo mv -f /tmp/fstab /etc/fstab
 
 enable_service tesla-mqtt.service
 
 echo "### Autoremove unneeded dependencies"
-sudo apt autoremove --purge -y
+do_exec sudo apt autoremove --purge -y
 
 echo "### Doing some housekeeing now"
-sudo journalctl --vacuum-time=1s
-sudo rm -f /var/backups/*.gz
-sudo apt clean
+do_exec sudo journalctl --vacuum-time=1s
+do_exec sudo rm -f /var/backups/*.gz
+do_exec sudo apt clean
 
 echo "### Activating overlay filesystem to make SD card read-only to prevent failure"
-sudo raspi-config nonint do_overlayfs 0
+do_exec sudo raspi-config nonint do_overlayfs 0
 
-history -c && history -w
+do_exec history -c && history -w
 
 echo "### SUCCESS all done, press ENTER to reboot"
 read
